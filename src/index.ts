b499d8d989db3862c460f9c63d8c1a8fee6e708a
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-07-18 06:47:59 -0300

    Start Project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import cors from "cors";
+import express, { json } from "express";
+import "express-async-errors"
+import "./config/config.js"
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+const port: number = +process.env.PORT || 5000;
+
+app.listen(port, () => {
+	console.log(`Server is listening on port ${port}.`);
+})
