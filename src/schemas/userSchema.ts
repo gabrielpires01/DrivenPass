494001e7de9e1096bc41bae332f0568d113ad503
Rodrigodcs <rodrigodcsee@gmail.com>
2022-08-05 11:01:24 -0300

    Preparando revisão de código

--- src/schemas/userSchema.ts
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-import { NewUser } from "../respositories/userRepository.js";
-
-export const newUserSchema = Joi.object<NewUser>({
-	email: Joi.string().email().required(),
-	password: Joi.string().min(10).required()
-})
